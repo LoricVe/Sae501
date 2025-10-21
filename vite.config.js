ed1e6dd3388089258d94347259b1a90d01884231
LoricV <loricv16@gmail.com>
2025-10-21 13:17:34 +0200

    pour avoir mon site web github

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Remplacez 'nom-du-repo' par le nom de votre repository GitHub
+  base: process.env.NODE_ENV === 'production' ? '/Sae501/' : '/'
 })
